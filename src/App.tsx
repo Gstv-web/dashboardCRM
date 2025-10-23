7d0b8d8d96153e20b5c186a46da4f781a8307eb0
Gustavo Oliveira <gustavo.myw@live.com>
2025-10-23 14:28:55 -0300

    Iniciando estrutura do dashboard e componentes

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
   const [totalProspects, setTotalProspects] = useState(0)
   return (
     <>
-      <div className="flex flex-col items-center border-2 w-full h-full overflow-auto">
+      <div className="flex flex-col items-center border-2 w-full h-full overflow-auto bg-white">
         <h1 className="text-5xl font-bold underline p-4">Dashboard CRM</h1>
         <div className="flex flex-col gap-10 w-4/5 justify-center border-2">
           <h2 className="font-bold ml-4 mt-2">Visão Geral</h2>
