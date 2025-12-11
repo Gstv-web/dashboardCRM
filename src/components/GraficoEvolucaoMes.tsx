fc2de77a4f957a85bdc9b252647b82d241ade9b5
Gustavo Oliveira <gustavo.myw@live.com>
2025-12-11 14:17:05 -0300

    gerando lógica para reproduzir o mesmo comportamento no Gráfico de evolução do mês

--- src/components/GraficoEvolucaoMes.tsx
+++ src/components/GraficoEvolucaoMes.tsx
@@ -33,7 +33,7 @@ export default function GraficoEvolucaoMes({ dados, onPontoClick }: Props) {
   ];
 
   const etapas = Object.keys(dados[0]).filter((k) => k !== "dia" && k !== "itens");
-  const dias = Object.values(dados[0]);
+  const dias = Object.values(dados);
   console.log("printando 'Object.values':", dias);
   // console.log("dados no componente GraficoEvolucaoMes:", dados)
 
